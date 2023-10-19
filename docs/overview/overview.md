9c44891003f620d39bc57a163fe3582afa13d186
최윤석 <achoistic98@naver.com>
2023-10-19 20:50:17 +0900

    Init: Change project from goopt to acvii

--- docs/overview/overview.md
+++ docs/overview/overview.md
@@ -1,1 +1,9 @@
-# Overview
+# Insparation
+
+Istio AuthorizationPolicy resource has some issues.
+
+1. You had to delete and re-deploy to update changed AuthorizationPolicy
+2. You cannot figure out how AuthorizationPolicies are working, and how many AuthorizationPolicies are in cluster.
+3. When you have trouble with Istio AuthorizationPolicy, It is hard to recognize what is wrong with it.
+
+So, I started to develop this visualizer
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kind create cluster --config deployments/kind.yml
-kubectl create clusterrolebinding default-view --clusterrole=view --serviceaccount=default:goopt
-docker build -t achoistic98/goopt:v0.0.1 -f build/Dockerfile.goopt .
-docker push achoistic98/goopt:v0.0.1
-kubectl apply -f deployments/goopt.yml
+kubectl create clusterrolebinding default-view --clusterrole=view --serviceaccount=default:acvii
+docker build -t achoistic98/state:v0.0.1 -f build/Dockerfile.acvii .
+docker push achoistic98/acvii:v0.0.1
+kubectl apply -f deployments/acvii.yml
