7362249a2ff0593db39bc59f4dd860cc54037e23
최윤석 <achoistic98@naver.com>
2023-10-29 01:11:11 +0900

    Feat: Add sub-commands to start acvii in CLI

--- cmd/acvctl/acvctl/acvctl.go
+++ cmd/acvctl/acvctl/acvctl.go
@@ -13,6 +13,8 @@ func main() {
 		os.Exit(1)
 	}
 
+	cmd.Execute()
+
 	if err := cmd.Execute(); err != nil {
 		fmt.Println((err.Error()))
 		os.Exit(1)
--- cmd/acvctl/root.go
+++ cmd/acvctl/root.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -13,6 +16,7 @@ import (
 const (
 	// acvctlConfigPath = "$HOME/.acvctl/config.yml"
 	acvctlConfigPath = "../../../.acvctl/config.yml"
+	binaryFilePath   = "../../acvii/acvii"
 )
 
 var rootCmd = &cobra.Command{
@@ -24,20 +28,80 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+func New() *cobra.Command {
+	return rootCmd
+}
+
+var startCMD = &cobra.Command{
+	Use:   "start",
+	Short: "start",
+	Long:  "start server",
+	Run: func(cmd *cobra.Command, args []string) {
+		command := exec.Command(binaryFilePath)
+
+		command.Stdout = os.Stdout
+		command.Stderr = os.Stderr
+
+		err := command.Run()
+		if err != nil {
+			fmt.Println("실행 중 오류 발생:", err)
+		}
+	},
+}
+
 var installCMD = &cobra.Command{
 	Use:   "install",
-	Short: "install acvii pod",
-	Long:  "install acvii pod in Kubernetes cluster",
+	Short: "deploy acvii pod",
+	Long:  "deploy acvii pod in Kubernetes cluster",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Print("INSTALL COMMAND RUNNING")
 	},
 }
 
-// var acvctl = env.NewStringVar("CONFIG", "for initial configuration of acvctl", AcvctlConfigPath)
+var planCMD = &cobra.Command{
+	Use:   "plan",
+	Short: "check how AuthorizationPolicy gonna change",
+	Long:  "check how AuthorizationPolicy gonna change",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Print("PLAN COMMAND RUNNING")
+	},
+}
+
+var applyCMD = &cobra.Command{
+	Use:   "apply",
+	Short: "apply current AuthroizationPolicy settings",
+	Long:  "apply current AuthroizationPolicy settings",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Print("APPLY COMMAND RUNNING")
+	},
+}
+
+var dashboardCMD = &cobra.Command{
+	Use:   "dashboard",
+	Short: "show dashboard web UI in browser",
+	Long:  "show dashboard web UI in browser",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Print("DASHBOARD COMMAND RUNNING")
+	},
+}
+
+var uninstallCMD = &cobra.Command{
+	Use:   "uninstall",
+	Short: "delete acvii pod",
+	Long:  "delete acvii pod in Kubernetes cluster",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Print("UNINSTALL COMMAND RUNNING")
+	},
+}
 
 func init() {
 	env.ForceNewStringVar("CONFIG", "for initial configuration of acvctl", acvctlConfigPath)
 	rootCmd.AddCommand(installCMD)
+	rootCmd.AddCommand(planCMD)
+	rootCmd.AddCommand(applyCMD)
+	rootCmd.AddCommand(dashboardCMD)
+	rootCmd.AddCommand(uninstallCMD)
+	rootCmd.AddCommand(startCMD)
 }
 
 func Execute() error {
@@ -47,9 +111,7 @@ func Execute() error {
 
 func ConfigAcvctl() error {
 	acvctlPath := env.Get("CONFIG").DefaultValue
-	// if !ok {
-	// 	return errors.New("acvctl config file is not registered as env variables")
-	// }
+
 	configPath := filepath.Dir(acvctlPath)
 	baseName := filepath.Base(acvctlPath)
 	configType := filepath.Ext(acvctlPath)
